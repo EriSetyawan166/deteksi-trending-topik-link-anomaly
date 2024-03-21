328d13811e34217bbb97a3726d7bf9fe8d94d5bf
Muhammad Eri Setyawan <erisetyawan34@gmail.com>
2024-03-21 12:48:05 +0700

    fixing init

---
+++ util/__init__.py
@@ -0,0 +1,2 @@
+from .connection import connection
+from .connection import close_connection
--- util/connection.py
+++ util/connection.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-def connect_to_database(host, user, password, database):
+def connection(host, user, password, database):
     try:
         # Membuat koneksi ke database
         connection = mysql.connector.connect(
@@ -25,7 +25,7 @@ def close_connection(connection):
         print(f"Error: {e}")
 
 def main():
-    connection = connect_to_database("localhost", "root", "", "deteksi_trending_topik")
+    connection = connection("localhost", "root", "", "deteksi_trending_topik")
     close_connection(connection)
 
 if __name__ == "__main__":
