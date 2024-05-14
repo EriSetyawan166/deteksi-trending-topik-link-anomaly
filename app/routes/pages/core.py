8c1465d76088ede005da234b484e7ae6c422ec8d
Muhammad Eri Setyawan <erisetyawan34@gmail.com>
2024-05-14 13:14:40 +0700

    Add import route, API for data and delete all data, and upload CSV file functionality; update scraping route to import route; update custom styles for DataTables pagination and row alignment; update base template to include SweetAlert2 library;

--- app/routes/pages/core.py
+++ app/routes/pages/core.py
@@ -1,5 +1,6 @@
 # Flask modules
 from flask import Blueprint, render_template, current_app, Response, request
+import csv
 from ...util import link_anomaly
 # from models import DatasetPreprocessed
 import mysql.connector
@@ -27,10 +28,103 @@ def home_route():
     return render_template("pages/index.html")
 
 
-@core_bp.route("/scraping")
+@core_bp.route("/import")
 def scraping_route():
-    # Render the scraping template
-    return render_template("pages/scraping.html")
+    # Render the import template
+    return render_template("pages/import.html")
+
+
+@core_bp.route("/api/data")
+def api_data():
+    response_data = []
+    db = create_db_connection()
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT username, created_at, full_text FROM dataset_twitter")
+    data = cursor.fetchall()
+
+    for row in data:
+        # Each row is a tuple, so unpack the values
+        username, created_at, full_text = row
+        # Create a dictionary for the current row and append it to the response data list
+        row_data = {
+            "username": username,
+            "created_at": created_at,
+            "full_text": full_text,
+        }
+        response_data.append(row_data)
+
+    # Wrap the list of data in a dictionary under the key 'data'
+    return jsonify({"data": response_data})
+
+
+@core_bp.route("api/delete_all_data", methods=['DELETE'])
+def api_delete_all_data():
+    db = create_db_connection()
+    cursor = db.cursor()
+    cursor.execute(
+        "DELETE FROM dataset_twitter")
+    db.commit()
+    return jsonify({"message": "All data deleted successfully"})
+
+
+@core_bp.route("api/upload_csv_file", methods=['POST'])
+def api_upload_csv_file():
+    db = create_db_connection()
+    cursor = db.cursor()
+    if 'fileInput' not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
+
+    file = request.files['fileInput']
+
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    if file and file.filename.endswith('.csv'):
+        try:
+            # Simpan file CSV ke direktori server (misalnya 'uploads/')
+            file.save('tweets-data/' + file.filename)
+
+            # Proses file CSV
+            with open('tweets-data/' + file.filename, 'r', encoding='utf-8') as csvfile:
+                csvreader = csv.DictReader(csvfile)
+                for row in csvreader:
+                    # Lakukan sesuatu dengan setiap baris (contoh: simpan ke database)
+                    conversation_id_str = row['conversation_id_str']
+                    created_at = row['created_at']
+                    favorite_count = row['favorite_count']
+                    full_text = row['full_text']
+                    id_str = row['id_str']
+                    image_url = row['image_url']
+                    in_reply_to_screen_name = row['in_reply_to_screen_name']
+                    lang = row['lang']
+                    location = row['location']
+                    quote_count = row['quote_count']
+                    reply_count = row['reply_count']
+                    retweet_count = row['retweet_count']
+                    tweet_url = row['tweet_url']
+                    user_id_str = row['user_id_str']
+                    username = row['username']
+
+                    # Query SQL untuk memasukkan data ke tabel
+                    sql = """INSERT INTO dataset_twitter (conversation_id_str, created_at, favorite_count, full_text, id_str, 
+                            image_url, in_reply_to_screen_name, lang, location, quote_count, reply_count, retweet_count, 
+                            tweet_url, user_id_str, username) 
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+                    values = (conversation_id_str, created_at, favorite_count, full_text, id_str, image_url, 
+                            in_reply_to_screen_name, lang, location, quote_count, reply_count, retweet_count, 
+                            tweet_url, user_id_str, username)
+                    
+                    # Eksekusi query SQL
+                    cursor.execute(sql, values)
+                    db.commit()
+
+            return jsonify({"message": "CSV file uploaded and processed successfully"}), 200
+        except Exception as e:
+            return jsonify({"error": "Failed to process CSV file: " + str(e)}), 500
+    else:
+        return jsonify({"error": "Invalid file format, please upload a CSV file"}), 400
+
 
 # Route for preprocessing page
 
